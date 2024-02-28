001709d0ae8a578287e89840a7d7e164efc8deea
giria <joan@giria.com>
2024-02-28 13:38:24 +0000

    Initial Commit

---
+++ WiFiSSID/WiFiSSIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiSSIDApp.swift
+//  WiFiSSID
+//
+//  Created by joan Barrull on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WiFiSSIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
